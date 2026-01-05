135fa4e304faeaa90715f54bcdbb4a3fc3ae20d3
“Mariana <marysiq@hotmail.com>
2026-01-05 13:07:45 -0800

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,7 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': ,
+      'react-refresh': r,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
